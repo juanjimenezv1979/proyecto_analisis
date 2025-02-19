70c8dcbaf0540bed302d8c92062d80e86dd56359
Juan Camilo Jimenez Vasquez <89559838+juanjimenezv1979@users.noreply.github.com>
2025-02-19 10:35:07 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,21 @@
+import streamlit as st
 import numpy as np
 import pandas as pd
-import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import scipy.stats as stats
+import duckdb
+from datetime import datetime
+# Cargar datos desde DuckDB 
+@st.cache_data
+def load_data():
+    query = """
+    SELECT * FROM 'cobertura_movil_definitivo.csv'
+    """
+    df = duckdb.query(query).to_df()
+    return df
 
-st.header("Hola mundo")
+df = load_data()
+
+st.header("Hacia una Antioquia conectada: Diagnóstico de la cobertura digital en zonas rurales y urbanas en el departamento de Antioquia durante el tercer  trimestre del año 2023.")
