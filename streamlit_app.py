a2ba82b544a53cd8fe7b55dc056fe02e30e52fb6
Juan Camilo Jimenez Vasquez <89559838+juanjimenezv1979@users.noreply.github.com>
2025-02-19 10:40:40 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,3 +19,11 @@ def load_data():
 df = load_data()
 
 st.header("Hacia una Antioquia conectada: Diagnóstico de la cobertura digital en zonas rurales y urbanas en el departamento de Antioquia durante el tercer  trimestre del año 2023.")
+
+# Procesamiento de datos
+def preprocess_data(df):
+    df['fecha'] = pd.to_datetime(df['fecha'])  # Convertir fechas
+    df['total_tecnologias'] = df[['3G', '4G', '5G']].sum(axis=1)  # Crear nueva métrica
+    return df
+
+df = preprocess_data(df)
